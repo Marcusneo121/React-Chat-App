3beee3487e622e106bcab580d8cd6a339e218f79
Marcus Neo <marcus121neo@gmail.com>
2023-04-12 16:22:46 +0800

    Authentication Flow completed

--- src/App.js
+++ src/App.js
@@ -1,8 +1,37 @@
 import './App.css';
+import { useState, useEffect, useRef } from 'react';
 import { Auth } from './components/Auth';
 
+import Cookies from 'universal-cookie';
+const cookies = new Cookies();
+
 function App() {
-  return <div><Auth /></div>
+
+  const [isAuth, setIsAuth] = useState(cookies.get('auth-token'));
+  const [room, setRoom] = useState(null);
+
+  const roomInputRef = useRef(null);
+
+  if (!isAuth) {
+    return (
+      <div><Auth setIsAuth={setIsAuth} /></div>
+    );
+  } else {
+    return <div>
+      {room ?
+        <div>Chat</div>
+        : <div className='room'>
+          <label>Enter Room Name:</label>
+          <input ref={roomInputRef} />
+          <button onClick={() => setRoom(roomInputRef.current.value)}>
+            Enter Chat
+          </button>
+        </div>
+      }
+    </div>
+  }
+
+
 }
 
 export default App;
--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,9 +1,26 @@
 import { auth, provider } from '../firebase-config.js';
 import { signInWithPopup } from 'firebase/auth';
 
-export const Auth = () => {
+import Cookies from 'universal-cookie';
+const cookies = new Cookies();
+
+export const Auth = (props) => {
+
+    const setIsAuth = props.setIsAuth;
+
     const signInWithGoogle = async () => {
-        await signInWithPopup(auth, provider);
+        try {
+            const result = await signInWithPopup(auth, provider);
+            console.log(result);
+            cookies.set("auth-token", result.user.refreshToken);
+            setIsAuth(true);
+        } catch (err) {
+            console.error(err)
+        }
+    }
+
+    const removeToken = () => {
+        cookies.remove("auth-token");
     }
 
     return <div className="auth">
